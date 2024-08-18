841de21196be4a0e680dc874fd08c27054c15da0
Andrew DeSimone <andrewdesimone96@gmail.com>
2024-08-17 22:34:59 -0400

    Delete Parser to prepare for refactor

--- Parser.py
+++
@@ -1,9 +0,0 @@
-from Lexer import Lexer
-
-class Parser:
-
-    def __init__(self, fileName):
-        self.lexer = Lexer(fileName)
-        self.tokens = self.lexer.getTokens()
-        print([str(i) for i in self.tokens])
-    
